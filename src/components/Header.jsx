e2836fbf0411aa6a27c31f1575f9aec422ae09a4
Amang2004 <amangupta.ans@gmail.com>
2025-05-24 18:10:15 +0530

    edit the code file '3'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import 'boxicons/css/boxicons.min.css';
 
 const Header = () => {
+    
   return (
     <header className='flex justify-between items-center py-4 px-4 lg:px-20'>
       <h1 className='text-3xl md:text-4xl lg:text-5xl font-light m-0'>MC</h1>
@@ -20,7 +22,17 @@ const Header = () => {
 
 
 
-      <button></button>
+      <button className='md:hidden text-3xl p-2 z-50'> <i class='bx  bx-menu-filter'></i> </button>
+    <div id="" className=' hidden fixed top-16 bottom-0 right-0 left-0 p-5 md:hidden z-40 bg-black bg-opacity-70 backdrop-blur-md '>
+        <nav className='flex flex-col gap-6'>
+            <a href="#" className='text-base tracking-wider transition-colors hover:text-gray-300 z-50 '>Company</a>
+             <a href="#" className='text-base tracking-wider transition-colors hover:text-gray-300 z-50 '>Company</a>
+        <a href="#" className='text-base tracking-wider transition-colors hover:text-gray-300 z-50 '>Company</a>
+        <a href="#" className='text-base tracking-wider transition-colors hover:text-gray-300 z-50 '>Company</a>
+
+        </nav>
+    </div>
+   
     </header>
   )
 }
