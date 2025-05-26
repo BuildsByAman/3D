2b14b641dfa15d29b11225846b099b0adcff7e8c
Amang2004 <amangupta.ans@gmail.com>
2025-05-26 22:56:07 +0530

    edit the code file '5'

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import Header from './components/header'
+import React from "react";
+import Header from "./components/header";
+import Hero from "./components/Hero";
+
 
 const App = () => {
   return (
     <main>
       {/* gradient image */}
-      <img src="/gradient.png" className="absolute top-0 right-0 opacity-60 z-[-1]" alt="" />
+      <img
+        src="/gradient.png"
+        className="absolute top-0 right-0 opacity-60 z-[-1]"
+        alt=""
+      />
       {/* blur effect */}
       <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
-      
-      <Header />
-    </main>
-  )
-}
 
-export default App
+      <Header />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "boxicons/css/boxicons.min.css";
+
+function Hero() {
+  return (
+    <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h[calc(90vh-6rem)]">
+      <div className="max-w-xl ml-[5%] z-20 mt-[90%] md:mt[60%] lg:mt-0">
+        <div className="relative w-[95%]">
+          <div>
+             < i class='bx  bx-diamond'  ></i> introducing
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Hero;
